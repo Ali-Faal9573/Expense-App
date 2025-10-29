a241c6675c93dcdf6b25be561f57fe9c067d6cfa
Ali-Faal <faal.ali.faal@gmail.com>
2025-10-29 19:48:45 +0330

    move widgets to the widget folder

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -25,6 +25,8 @@ class _ExpensesState extends State<Expenses> {
       category: Category.leisure,
     ),
   ];
+
+
 
   @override
   Widget build(BuildContext context) {
