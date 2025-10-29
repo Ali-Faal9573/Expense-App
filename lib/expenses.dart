428857257b6dbb6705eba5485a6adcd7133d2e79
Ali-Faal <faal.ali.faal@gmail.com>
2025-10-29 18:01:01 +0330

    add expense list widget to expenses widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
@@ -29,7 +30,14 @@ class _ExpensesState extends State<Expenses> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [Text('The chart'), Text('Expenses list ...')],
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registredExpenses,
+            ),
+          ),
+        ],
       ),
     );
   }
