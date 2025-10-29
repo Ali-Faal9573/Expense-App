9a128dff98e0290012f10c6df5da63f4a50432a9
Ali-Faal <faal.ali.faal@gmail.com>
2025-10-29 20:22:40 +0330

    add expense item data in card

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -11,7 +11,25 @@ class ExpenseItem extends StatelessWidget {
     return Card(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Text(expense.title),
+        child: Column(
+          children: [
+            Text(expense.title),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    const Icon(Icons.alarm),
+                    SizedBox(width: 8),
+                    Text(expense.date.toString()),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
