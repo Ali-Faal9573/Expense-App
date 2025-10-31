2bea9d8eee35d6d7604f9c7932ce5eb8042c90c1
Ali-Faal <faal.ali.faal@gmail.com>
2025-10-31 12:28:27 +0330

    create new widget for new expense modal

---
+++ lib/widgets/new_expense.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() {
+    return _NewExpensesState() ;
+  }
+}
+
+class _NewExpensesState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
