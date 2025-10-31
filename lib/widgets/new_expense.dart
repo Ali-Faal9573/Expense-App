9611764c23df37a0dbb54021a7840be2545fa216
Ali-Faal <faal.ali.faal@gmail.com>
2025-10-31 20:09:45 +0330

    add dialog for show validation error message

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -37,8 +37,26 @@ class _NewExpensesState extends State<NewExpense> {
     if (_titleController.text.trim().isEmpty ||
         amountIsInvalid ||
         _selectedDate == null) {
-      // show error message
+      showDialog(
+        context: context,
+        builder: (ctx) => AlertDialog(
+          title: const Text('Invalid Input'),
+          content: const Text(
+            'Please make sure a valid title, amount, date and category was entered.',
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(ctx);
+              },
+              child: const Text('Ok'),
+            ),
+          ],
+        ),
+      );
+      return;
     }
+    // ...
   }
 
   @override
@@ -121,9 +139,7 @@ class _NewExpensesState extends State<NewExpense> {
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  _submitExpenseData;
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               ),
             ],
