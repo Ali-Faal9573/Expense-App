f1408270f0229ea8909c609230bc9aabf7b6f50e
Ali-Faal <faal.ali.faal@gmail.com>
2025-11-04 20:27:00 +0330

    show a message when there is no expense to show

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -49,6 +49,16 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter ExpenseTracker'),
@@ -62,7 +72,7 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           const Text('The chart'),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses,onRemoveExpense: _removeExpense,)),
+          Expanded(child: mainContent),
         ],
       ),
     );
